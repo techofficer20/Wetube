2be27dd7517cf36b0249bc60a7d31486d5e9acb8
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-10 16:03:21 +0900

    2.6 Express Core: Middlewares

--- index.js
+++ index.js
@@ -30,8 +30,24 @@ function handleProfile(req, res) {
 }
 */
 const handleProfile = (req, res) => res.send("You are on my profile");
+const betweenHome = (req, res, next) => {
+  console.log("Between");
+  next();
+};
+/* middleware란 처리가 끝날 때 까지 연결되어 있는 것
+request는 어떻게 시작? 웹사이트에 접속하려 할 때부터..
+유저와 마지막 응답 사이에 존재하는 것을 middleware라 함
+express의 모든 함수는 middleware가 될 수 있음.
+express의 모든 route와 그런 것들은 request, response, next를 인자로 가지고 있는데
+middleware는 이때 response에 해당됨.
+res.send()를 마지막에 하지 않는다면 계속 로딩 화면이 뜰 것.
+우리가 원하는 만큼 middleware를 사용할 수 있음.
+모든 요청에 middleware 설정 방법: app.use() // get함수(route처리) 이전에 먼저 설정
+*/
+app.use(betweenHome);
+
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
 
-app.listen(4000, handleListening);
 // listen하기 시작할 때 handleListening 함수를 실행하게 됨 (콜백 함수)
+app.listen(PORT, handleListening);
