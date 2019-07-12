65ce42fab1937ebcb5f3649432b117dcc8f2ff03
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-12 21:31:17 +0900

    2.10 MVC Pattern part Two

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import bodyParser from "body-parser";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 const app = express(); // app 상수에 express를 실행해서 담음.
 
 app.use(cookieParser());
@@ -15,6 +16,8 @@ app.use(bodyParser.urlencoded({ extended: true })); // html
 app.use(helmet()); // node.js 앱 보안에 도움
 app.use(morgan("dev")); // logging (무슨 일이 어디서 일어났는지 기록, 인자로 로깅 옵션 설정 가능)
 
-app.use("/", globalRouter);
-app.use("/user", userRouter);
-app.use("/video", videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
+
+export default app;
