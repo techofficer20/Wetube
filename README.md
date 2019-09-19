fe4901ddd16520894159e34acc218f8c73f09d58
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-09-19 12:04:47 +0900

    restart setting

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # wetube
- 노마드코더 유튜브 클론코딩 레포지토리입니다.
+ 노마드코더 유튜브 클론코딩 레포지토리입니다. 2019년 09월 16일 월요일부터 restart
 # Pages:
 - [ ] Home
-- [x] Join
-- [x] Login
-- [x] Search
+- [] Join
+- [] Login
+- [] Search
 - [ ] User Detail
 - [ ] Edit Profile
 - [ ] Change Password
--- controllers/userController.js
+++
@@ -1,6 +0,0 @@
-export const join = (req, res) => res.render("join", {pageTitle: "Join"});
-export const login = (req, res) => res.render("login", {pageTitle: "Log In"});
-export const logout = (req, res) => res.render("logout", {pageTitle: "Log Out"});
-export const userDetail = (req, res) => res.render("userDetail", {pageTitle: "User Detail"});
-export const editProfile = (req, res) => res.render("editProfile", {pageTitle: "Edit Profile"});
-export const changePassword = (req, res) => res.render("changePassword", {pageTitle: "Change Password"});
