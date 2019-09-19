8588c1f63878fd3e413e6845836b6b8f9052cbc6
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-09-19 13:13:17 +0900

    2.7 Express Core: Middlewares part Two

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // const express = require("express"); babel 필요 X
 import express from "express"; // babel 필요
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 const PORT = 4000;
 
@@ -23,11 +28,12 @@ function handleProfile(req, res){
 */
 const handleProfile = (req, res) => res.send("You are on my profile");
 
-const betweenHome = (req, res, next) => {
-    console.log("Between");
-    next();
-}
-app.use(betweenHome);
-app.get("/", betweenHome, handleHome);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(helmet());
+app.use(morgan("dev")); // tiny, combined, common, dev..
+
+app.get("/", handleHome);
 app.get("/profile", handleProfile);
 app.listen(4000, handleListening);
